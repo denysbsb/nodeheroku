317b29e912354abbbc4ddb41ef633ac5fd636a3e
denysbsb <denys.design@gmail.com>
2018-02-14 15:15:20 -0200

    entrada de logs

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+var alert = require('alert-node');
 
-app.get('/thanks/webhook/', (req, res) =>
- alert('hello word');
-res.send('Hello World!')
-)
+app.get('/thanks/webhook/', function(req, res) {
+var nome = 0;
+console.log('Teste DENYS');
+res.send('Hello World!'+ nome+1);
+})
 
 var porta = process.env.PORT || 8080;
 app.listen(porta, () => console.log('Example app listening on port 3000!'))
