52a2ff7f8da397bc45f24065a6ae9ea9b7f9b526
denysbsb <denys.design@gmail.com>
2018-02-14 15:22:32 -0200

    thank boot

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ app.get('/', function(req, res) {
 res.send('Hello World!');
 })
 
+app.get('/thanks/boot', function(req,res){
+res.send('thanks boot');
+})
+
 var porta = process.env.PORT || 8080;
 app.listen(porta, () => console.log('Example app listening on port 3000!'))
