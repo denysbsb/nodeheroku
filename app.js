3b2b15325c9a3d41f3f1d72b37d710e52bdee8a2
denysbsb <denys.design@gmail.com>
2018-02-14 14:58:30 -0200

    module node

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/thanks/webhook/', (req, res) =>
+ alert('hello word');
+res.send('Hello World!')
+)
+
 var porta = process.env.PORT || 8080;
 app.listen(porta, () => console.log('Example app listening on port 3000!'))
