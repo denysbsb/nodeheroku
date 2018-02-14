fcc1d88c33bfd699e4116da8c1eb12702a099376
denysbsb <denys.design@gmail.com>
2018-02-14 16:14:00 -0200

    body parser

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+
+function verifyRequestSignature(req, res, buf) {
+    var signature = req.headers['x-hub-signature'];
+
+    if (!signature) {
+		// For testing, let's log an error. In production, you should throw an error.
+        console.error('Couldn\'t validate the signature.');
+    } else {
+        var elements = signature.split('=');
+        var signatureHash = elements[1];
+
+        var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+			.update(buf)
+			.digest('hex');
+
+        if (signatureHash != expectedHash) {
+            throw new Error('Couldn\'t validate the request signature.');
+        }
+    }
+}
 
 app.get('/', function(req, res) {
 res.send('Hello World!');
