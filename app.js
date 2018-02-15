a34db65f53039555e2331232c83d566cb5ec4f69
denysbsb <denys.design@gmail.com>
2018-02-15 15:24:32 -0200

    pg connect2

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var access_token = 'DQVJ2eWpIdy15Ylc0bmpJWFV0alhzX2lWOW9vaWRfVDhZAclZAYdUJTN2hxR
 
 const { Client } = require('pg');
 
+const pg = require('pg');
+
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
