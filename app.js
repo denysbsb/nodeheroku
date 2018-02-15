a2b6643dcd1d3962db14708af38f8219743c5f75
denysbsb <denys.design@gmail.com>
2018-02-15 16:34:39 -0200

    teste de envio para o callback

--- app.js
+++ app.js
@@ -193,53 +193,52 @@ app.post('/thanks/webhook', function(request, response) {
                                 console.log('result insert', result);
                             });
 
-                            var queryselect = `SELECT * FROM thanks WHERE create_date > now() - INTERVAL 1 week;`;
                             client.connect();
 
-                            client.query(query, (err, result) => {
+                            client.query(`SELECT * FROM thanks WHERE create_date > now() - INTERVAL '1 week';`, (err, result) => {
                                 console.log('client', result);
-                            //     if (err) {
-                            //         console.error('DEU err', err);
-                            //     } else if (result) {
-                            //         var summary = 'Thanks received!\n';
-                            //         // iterate through result rows, count number of thanks sent
-                            //         var sender_thanks_sent = 0;
-                            //         result.rows.forEach(function(row) {
-                            //             if(row.sender == sender) sender_thanks_sent++;
-                            //         });
-                            //         summary += `@[${sender}] has sent ${sender_thanks_sent} thanks in the last ${interval}\n`;
+                                if (err) {
+                                    console.error('DEU err', err);
+                                } else if (result) {
+                                    var summary = 'Thanks received!\n';
+                                    // iterate through result rows, count number of thanks sent
+                                    var sender_thanks_sent = 0;
+                                    result.rows.forEach(function(row) {
+                                        if(row.sender == sender) sender_thanks_sent++;
+                                    });
+                                    summary += `@[${sender}] has sent ${sender_thanks_sent} thanks in the last ${interval}\n`;
                                     
-                            //         // Iterate through recipients, count number of thanks received
-                            //         recipients.forEach(function(recipient) {
-                            //             let recipient_thanks_received = 0;
-                            //             result.rows.forEach(function(row) {
-                            //                 if(row.recipient == recipient) recipient_thanks_received++;
-                            //             });
-                            //             if(managers[recipient]) {
-                            //                 summary += `@[${recipient}] has received ${recipient_thanks_received} thanks in the last ${interval}. Heads up to @[${managers[recipient]}].\n`;
-                            //             } else {
-                            //                 summary += `@[${recipient}] has received ${recipient_thanks_received} thanks in the last ${interval}. I don't know their manager.\n`;
-                            //             }
-                            //         });
-                            //     }
+                                    // Iterate through recipients, count number of thanks received
+                                    recipients.forEach(function(recipient) {
+                                        let recipient_thanks_received = 0;
+                                        result.rows.forEach(function(row) {
+                                            if(row.recipient == recipient) recipient_thanks_received++;
+                                        });
+                                        if(managers[recipient]) {
+                                            summary += `@[${recipient}] has received ${recipient_thanks_received} thanks in the last ${interval}. Heads up to @[${managers[recipient]}].\n`;
+                                        } else {
+                                            summary += `@[${recipient}] has received ${recipient_thanks_received} thanks in the last ${interval}. I don't know their manager.\n`;
+                                        }
+                                    });
+                                }
                                 
-                            //     var GRAPH_URL_MESSAGES = 'https://graph.facebook.com' + '/' + mention_id + '/comments';
-                            //     var request = require('request');
+                                var GRAPH_URL_MESSAGES = 'https://graph.facebook.com' + '/' + mention_id + '/comments';
+                                var request = require('request');
 
-                            //     request({
-                            //     url: GRAPH_URL_MESSAGES,
-                            //     // proxy: proxyMaquina,
-                            //     method: 'POST',
-                            //     qs: {
-                            //         message: summary
-                            //     },
-                            //     headers: {
-                            //         Authorization: 'Bearer ' + access_token
-                            //     },
-                            //     json: true
-                            //     }, function (err, res, body) {
-                            //         console.log('Comment reply', mention_id);
-                            //     });
+                                request({
+                                url: GRAPH_URL_MESSAGES,
+                                // proxy: proxyMaquina,
+                                method: 'POST',
+                                qs: {
+                                    message: summary
+                                },
+                                headers: {
+                                    Authorization: 'Bearer ' + access_token
+                                },
+                                json: true
+                                }, function (err, res, body) {
+                                    console.log('Comment reply', mention_id);
+                                });
                             });
 
                             console.log(1111111);
